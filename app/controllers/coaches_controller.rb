ef1e3c1c7b6d58084d715ebcfd4b65c99480037f
Jeffrey Tong <trigun0x2@gmail.com>
2014-03-07 18:28:15 -0700

    added show page for coach pages

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -3,6 +3,10 @@ class CoachesController < ApplicationController
     @coaches = Coach.where( active: true )
   end
 
+  def show
+    @coach = Coach.find(params[:id])
+  end
+
   def new
    @coach = Coach.new
   end
